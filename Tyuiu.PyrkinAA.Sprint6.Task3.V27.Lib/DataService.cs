c8f4fb2bf9119fa8433d925a0b8929df31ec20f5
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-24 17:55:21 +0500

    1

--- Tyuiu.PyrkinAA.Sprint6.Task3.V27.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint6.Task3.V27.Lib/DataService.cs
@@ -11,20 +11,20 @@ namespace Tyuiu.PyrkinAA.Sprint6.Task3.V27.Lib
             int rows = matrix.GetLength(0);
             int cols = matrix.GetLength(1);
 
-          
+
             int[,] result = (int[,])matrix.Clone();
 
-         
+
             List<int> fourthColumn = new List<int>();
             for (int i = 0; i < rows; i++)
             {
                 fourthColumn.Add(matrix[i, 3]);
             }
 
-        
+
             fourthColumn.Sort();
 
-           
+
             for (int i = 0; i < rows; i++)
             {
                 result[i, 3] = fourthColumn[i];
