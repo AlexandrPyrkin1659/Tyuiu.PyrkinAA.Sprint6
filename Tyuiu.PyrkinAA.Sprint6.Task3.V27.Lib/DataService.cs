802d23f20c8ed1388757a5015ede2dcfd382a93a
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-24 17:23:06 +0500

    1

--- Tyuiu.PyrkinAA.Sprint6.Task3.V27.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint6.Task3.V27.Lib/DataService.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.PyrkinAA.Sprint6.Task3.V27.Lib
 {
-    public class DataService
+    public class DataService : ISprint6Task3V27
     {
         public int[,] Calculate(int[,] matrix)
         {
             int rows = matrix.GetLength(0);
             int cols = matrix.GetLength(1);
 
-         
-
+            // Преобразуем матрицу в список строк для сортировки
             var rowsList = new List<int[]>();
 
             for (int i = 0; i < rows; i++)
@@ -24,10 +24,10 @@ namespace Tyuiu.PyrkinAA.Sprint6.Task3.V27.Lib
                 rowsList.Add(row);
             }
 
-           
+            // Сортируем по 4-му столбцу (индекс 3)
             rowsList.Sort((row1, row2) => row1[3].CompareTo(row2[3]));
 
-            
+            // Создаем новую матрицу с отсортированными строками
             int[,] result = new int[rows, cols];
 
             for (int i = 0; i < rows; i++)
